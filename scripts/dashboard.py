71c63b65b28756d9a89172a02d41a1b69b2c07e2
FriskyDingo <leveyryan28@gmail.com>
2024-11-08 10:03:00 -0500

    temp for web ui

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -7,7 +7,8 @@ db = client["supply_chain_db"]
 
 @app.route('/')
 def index():
-    return "Supply Chain Dashboard Running!"
+    return render_template('index.html')
+    #return "Supply Chain Dashboard Running!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
